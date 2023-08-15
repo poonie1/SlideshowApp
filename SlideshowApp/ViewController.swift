5c7867b3f90a2a03b16564cf620e3cb32dcdda21
Poonie1 <cables.brokers0t@icloud.com>
2023-08-15 23:05:55 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 大森宗一郎 on 2023/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
